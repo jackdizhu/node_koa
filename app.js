fd7b139caf2d39f792fa3755abadf6e2ed07cca4
jackdizhu <376365334@qq.com>
2018-01-23 18:33:44 +0800

    koa2 init

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-var app = require('koa')()
-  , logger = require('koa-logger')
-  , json = require('koa-json')
-  , cors = require('koa-cors')
-  , views = require('koa-views')
-  , onerror = require('koa-onerror');
+const Koa = require('koa')
+const app = new Koa()
+const cors = require('koa-cors')
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var api = require('./routes/api');
+const index = require('./routes/index')
+const users = require('./routes/users')
 
 // error handler
-onerror(app);
+onerror(app)
 
-// global middlewares
-app.use(views('views', {
-  root: __dirname + '/views',
-  default: 'ejs'
-}));
-app.use(require('koa-bodyparser')());
-app.use(json());
-app.use(cors()); // api 服务器 允许跨域
-app.use(logger());
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(cors()) // api 服务器 允许跨域
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
-});
+app.use(views(__dirname + '/views', {
+  extension: 'ejs'
+}))
 
-app.use(require('koa-static')(__dirname + '/public'));
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
 
-// routes definition
-app.use(index.routes(), index.allowedMethods());
-app.use(users.routes(), users.allowedMethods());
-app.use(api.routes(), api.allowedMethods());
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 });
 
-module.exports = app;
+module.exports = app
