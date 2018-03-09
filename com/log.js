58bda429b19517ebb52c2c06024518577916e25f
jackdizhu <376365334@qq.com>
2018-03-09 21:13:09 +0800

    log 修改

--- com/log.js
+++ com/log.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 const path = require('path')
 
 // 配置 绝对路径
-var filename = path.resolve(__dirname, '../log/koa2-log.js')
+var filename = path.resolve(__dirname, '../log/koa2-log.log')
 var logger = function (Obj) {
   var data = new Date().toLocaleString() + '--\n  ' + JSON.stringify(Obj) + '\n'
   fs.appendFile(filename, data, {encoding: 'utf-8'}, function () {
