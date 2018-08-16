1884e6897505f806d550b73f445eaa35a36ef7f4
jackdizhu <376365334@qq.com>
2018-08-16 16:48:47 +0800

    处理 建表 错误

--- models/user.js
+++ models/user.js
@@ -1,45 +1,9 @@
 // user.js
 
-var Sequelize = require('sequelize')
-var DB = require('../database/index')
-var UserChildrenModal = require('./userChildren')
-
-// 创建 model
-var User = DB.define('user', {
-  userChildrenId: {
-    type: Sequelize.STRING
-  },
-  userName: {
-    type: Sequelize.STRING, // 指定值的类型
-    field: 'user_name' // 指定存储在表中的键名称
-  },
-  // 没有指定 field，表中键名称则与对象键名相同，为 password
-  password: {
-    type: Sequelize.STRING
-  }
-},
-{
-  // 如果为 true 则表的名称和 model 相同，即 user
-  // 为 false MySQL创建的表名称会是复数 users
-  // 如果指定的表名称本就是复数形式则不变
-  freezeTableName: true
-})
-
-// 建立表 关联
-// 指定 User 和 UserChildren 的关系为 1：1 的关系 User.userChildrenId === UserChildren.id
-// User.belongsTo(UserChildrenModal.User, {foreignKey: 'userChildrenId', targetKey: 'id'})
-// 指定 User 和 UserChildren 的关系为 1 : n 的关系 User.id === UserChildren.userId
-User.hasMany(UserChildrenModal.User, {foreignKey: 'id', targetKey: 'userId'})
-
-// 创建表
-// User.sync() 会创建表并且返回一个Promise对象
-// 如果 force = true 则会把存在的表（如果users表已存在）先销毁再创建表
-// 默认情况下 forse = false
-User.sync({
-  force: false
-})
-
-exports.User = User
+// var Sequelize = require('sequelize')
+// var DB = require('../database/index')
+let UserChildren = require('./index').UserChildren
+let User = require('./index').User
 
 // 添加新用户
 exports.insert = function ({userName, password, userChildrenId}) {
@@ -62,7 +26,7 @@ exports.findOne = function (where) {
 exports.findOneInclude = function (where) {
   return User.findOne({
     include: [{
-      model: UserChildrenModal.User,
+      model: UserChildren.User,
       'where': where
     }]
   })
@@ -81,7 +45,7 @@ exports.findInclude = function (where, whereChildren) {
     'where': where,
     include: [
       {
-        model: UserChildrenModal.User,
+        model: UserChildren.User,
         'where': whereChildren
       }
     ]
--- models/userChildren.js
+++ models/userChildren.js
@@ -1,44 +1,14 @@
 // user.js
 
-var Sequelize = require('sequelize')
-var DB = require('../database/index')
+// var Sequelize = require('sequelize')
+// var DB = require('../database/index')
+let UserChildren = require('./index').UserChildren
 // var UserModal = require('./user')
-
-// 创建 model
-var User = DB.define('userChildren', {
-  userId: {
-    type: Sequelize.STRING
-  },
-  userName: {
-    type: Sequelize.STRING, // 指定值的类型
-    field: 'user_name' // 指定存储在表中的键名称
-  },
-  // 没有指定 field，表中键名称则与对象键名相同，为 password
-  password: {
-    type: Sequelize.STRING
-  }
-},
-{
-  // 如果为 true 则表的名称和 model 相同，即 user
-  // 为 false MySQL创建的表名称会是复数 users
-  // 如果指定的表名称本就是复数形式则不变
-  freezeTableName: true
-})
-
-// 创建表
-// User.sync() 会创建表并且返回一个Promise对象
-// 如果 force = true 则会把存在的表（如果users表已存在）先销毁再创建表
-// 默认情况下 forse = false
-User.sync({
-  force: false
-})
-
-exports.User = User
 
 // 添加新用户
 exports.insert = function ({userName, password, userId}) {
   // 向 user 表中插入数据
-  return User.create({
+  return UserChildren.create({
     userId: userId,
     userName: userName,
     password: password
@@ -47,26 +17,26 @@ exports.insert = function ({userName, password, userId}) {
 
 // 通过用户名查找用户
 exports.findOne = function (where) {
-  return User.findOne({
+  return UserChildren.findOne({
     where: where
   })
 }
 
 // 通过用户名查找用户
 exports.find = function (where) {
-  return User.findAll({
+  return UserChildren.findAll({
     where: where
   })
 }
 
 // 通过 ID 查找
 exports.findById = function (id) {
-  return User.findById(id)
+  return UserChildren.findById(id)
 }
 
 // 通过 ID 查找
 exports.update = function (user) {
-  return User.update({
+  return UserChildren.update({
     userName: user.userName,
     password: user.password
   }, {
